cfb8528d961f935019ab788198e71b4036ba6087
murari_kaushal <murari.kaushal@bizbrolly.com>
2023-02-20 17:01:49 +0530

    backend

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const { connection } = require("./db")
+const {userRouter} = require("./routes/user.route")
+const {postRouter} = require("./routes/post.route")
+const { authenticate } = require("./middleware/authenti")
+require("dotenv").config()
+const app  = express()
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+    res.send("hellooooo")
+})
+
+app.use("/users",userRouter)
+app.use(authenticate)
+app.use("/posts",postRouter)
+
+app.listen(process.env.port,async()=>{
+    try {
+        await connection
+        console.log("db is connected")
+    } catch (error) {
+        console.log(error)
+    }
+    console.log(" server connect")
+})
